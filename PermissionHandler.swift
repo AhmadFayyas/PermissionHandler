1ca347a0eb7106bba6d70067645485d9b4b08ebe
AhmadFayyas <ahmadf91@programmer.net>
2018-03-01 16:57:39 +0200

    minor fixes

--- PermissionHandler.swift
+++ PermissionHandler.swift
@@ -3,7 +3,6 @@
 //  Copyright © 2018 Yousef Younes. All rights reserved.
 //
 
-
 import Foundation
 import Contacts
 import AddressBook
@@ -13,7 +12,7 @@ import EventKit
 import UserNotifications
 
 //Enum used to specify which permission to request, add more to it and you can handle its call within _requestAccess_
-public enum PermissionType {
+enum PermissionType {
     case contacts
     case photos
     case events
@@ -23,86 +22,80 @@ public enum PermissionType {
 }
 
 /**
- Author: Yousef Younis
+ A simple iOS permission handler in Swift
+ 
+ - Author: Yousef Younis
  */
 class PermissionHandler {
     
     /**
-      The main access point to this handler, in which you can specify which permission to request and a completion handler for the return value of the permission status.
+     The main access point to this handler, in which you can specify which permission to request and a completion handler for the return value of the permission status.
      
      - parameter permissionType: an enum value of type PermissionType to specify which permission to be handled
      
      - parameter completionHandler: a handler with a return value of Bool indicating if the permission is granted or not
+     -  accessGranted: the retunred Bool for indicating whether the permission is granted or not
      
      - Author: Yousef Younis
      */
-
+    
     class func requestAccess(permissionType:PermissionType, completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
         // call the corresponding function for each permission
         switch permissionType {
         case .contacts:
             requestContactsPermission(completionHandler)
-            break
+            
         case .photos:
             requestPhotoLibraryAccess(completionHandler)
-            break
+            
         case .reminders:
             requestRemindersPermission(completionHandler)
-            break
+            
         case .events:
             requestEventsPermission(completionHandler)
-            break
+            
         case .notification:
             requestNotificationAccess(completionHandler)
-            break
         }
     }
     
-   
-    /// All of the below code is called privatly by _requestAccess_
-  
+    /* All of the below code is called privately by _requestAccess_ */
     
-    //handle granting and checking contacts access permission
-    fileprivate class func requestContactsPermission( _ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+    /// handles granting and checking contacts access permission
+    private class func requestContactsPermission( _ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
         CNContactStore().requestAccess(for: .contacts) { granted, error in
             completionHandler(granted)
         }
     }
     
-    //handle granting and checking Photo/Video acceess permission
-    fileprivate class func requestPhotoLibraryAccess( _ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+    /// handle granting and checking Photo/Video acceess permission
+    private class func requestPhotoLibraryAccess( _ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
         PHPhotoLibrary.requestAuthorization({ (photosStatus:PHAuthorizationStatus) in
-            if photosStatus == PHAuthorizationStatus.authorized {
-                completionHandler(true)
-            } else {
-                completionHandler(false)
-            }
+            completionHandler(photosStatus == .authorized ? true : false)
         })
     }
     
-    //handle granting and checking Events access permission, referred to by the OS as Calendar
-    fileprivate class func requestEventsPermission( _ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+    /// handles granting and checking Events access permission, referred to by the OS as Calendar
+    private class func requestEventsPermission( _ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
         EKEventStore().requestAccess(to: .event, completion: { (granted, error) in
             completionHandler(granted)
         })
     }
     
-    //handle granting and checking Reminders access permission, which uses the same EKEventStore as Events but is handled with seperate permissions and reading/writing methods
-    fileprivate class func requestRemindersPermission( _ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+    /// handles granting and checking Reminders access permission, which uses the same EKEventStore as Events but is handled with seperate permissions and reading/writing methods
+    private class func requestRemindersPermission( _ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
         EKEventStore().requestAccess(to: .reminder, completion: { (granted, error) in
             completionHandler(granted)
         })
         
     }
     
-   
-    //handles notification access permission
-    fileprivate class func requestNotificationAccess( _ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
-             UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]) { (granted, error) in
-                completionHandler(granted)
-            }
+    
+    ///handles notification access permission
+    private class func requestNotificationAccess( _ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]) { (granted, error) in
+            completionHandler(granted)
+        }
     }
 }
-
-
 
